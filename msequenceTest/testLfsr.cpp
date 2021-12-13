22c8011efd9295fb5ac4bff1473660fd137cfc7d
Matsuyanagi <1475814+Matsuyanagi@users.noreply.github.com>
2021-12-14 01:39:29 +0900

    Test4Bit など TestLessEq20Bit に含まれる余計なテストを削除
    テストのループを mnumber を外側へ移動。
    mnumber 初期値を 1,2, ... のほかに 2^n-1, -2, -3, ... を追加

--- msequenceTest/testLfsr.cpp
+++ msequenceTest/testLfsr.cpp
@@ -1,4 +1,4 @@
-#include "pch.h"
+﻿#include "pch.h"
 #include "../msequence/lfsr.h"
 #include "../msequence/UsingBitRecorder.h"
 
@@ -23,82 +23,30 @@ TEST( TestLfsr, Test3Bit ) {
 	EXPECT_EQ(recorder->GetNumberOfSetBits(),1<<bit_num);
 }
 
-TEST( TestLfsr, Test4Bit ) {
-	const int bit_num=4;
-
-	LfsrParams lfsr_param = find_lfsr_param( bit_num );
-
-	uint64_t mnumber = 1;
-	const uint64_t mseq_bit_length = 1LL << bit_num;
-
-	UsingBitRecorder *recorder = new UsingBitRecorder(1<<bit_num);
-	recorder->SetBit(0);
-
-	for (uint64_t i = 0; i < mseq_bit_length-1; i++)
-	{
-		recorder->SetBit(mnumber);
-		mnumber = mseq( mnumber, bit_num, lfsr_param.taps );
-	}
-	recorder->SetBit(mnumber);
-	EXPECT_TRUE(recorder->IsAllSet());
-	EXPECT_EQ(recorder->GetNumberOfSetBits(),1<<bit_num);
-}
-
-TEST( TestLfsr, Test8Bit ) {
-	const int bit_num=8;
-
-	LfsrParams lfsr_param = find_lfsr_param( bit_num );
-
-	uint64_t mnumber = 1;
-	const uint64_t mseq_bit_length = 1LL << bit_num;
-
-	UsingBitRecorder *recorder = new UsingBitRecorder(1<<bit_num);
-	recorder->SetBit(0);
-
-	for (uint64_t i = 0; i < mseq_bit_length-1; i++)
-	{
-		recorder->SetBit(mnumber);
-		mnumber = mseq( mnumber, bit_num, lfsr_param.taps );
-	}
-	recorder->SetBit(mnumber);
-	EXPECT_TRUE(recorder->IsAllSet());
-	EXPECT_EQ(recorder->GetNumberOfSetBits(),1<<bit_num);
-}
-
-TEST( TestLfsr, Test16Bit ) {
-	const int bit_num=16;
-
-	LfsrParams lfsr_param = find_lfsr_param( bit_num );
-
-	uint64_t mnumber = 1;
-	const uint64_t mseq_bit_length = 1LL << bit_num;
-
-	UsingBitRecorder *recorder = new UsingBitRecorder(1<<bit_num);
-	recorder->SetBit(0);
-
-	for (uint64_t i = 0; i < mseq_bit_length-1; i++)
-	{
-		recorder->SetBit(mnumber);
-		mnumber = mseq( mnumber, bit_num, lfsr_param.taps );
-	}
-	recorder->SetBit(mnumber);
-	EXPECT_TRUE(recorder->IsAllSet());
-	EXPECT_EQ(recorder->GetNumberOfSetBits(),1<<bit_num);
-	delete recorder;
-}
-
 TEST( TestLfsr, TestLessEq20Bit ) {
 
-	std::vector<uint64_t> mnumbers = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 };
-	for (auto mnumber : mnumbers)
+	std::vector<uint64_t> mnumbers_org = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 };
+	for (auto const &lfsr_param : lfsr_params)
 	{
-		for (auto const &lfsr_param : lfsr_params)
+		const int bit_num = lfsr_param.bit_size;
+		if (bit_num>20) continue;
+		std::vector<uint64_t> mnumbers = mnumbers_org;
+		const uint64_t max = 1LL << bit_num;
+
+		// 初期値に大きい数値を入れる
+		for (size_t i = 1; i <= 16; i++)
 		{
-			int bit_num = lfsr_param.bit_size;
-			if (bit_num>20) continue;
+			if (max-i == 0){
+				continue;
+			}
+			mnumbers.emplace_back(max-i);
+		}
+
+		for (auto mnumber : mnumbers)
+		{
 			if (mnumber >= (1LL<<bit_num) ) continue;
 
-			LfsrParams lfsr_param = find_lfsr_param( bit_num );
+			// LfsrParams lfsr_param = find_lfsr_param( bit_num );
 
 			const uint64_t mseq_bit_length = 1LL << bit_num;
 
